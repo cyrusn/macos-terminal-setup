92c14800977c3afb9cc35ca163b7bb75f2d919e9
cyrusn <cyrusncy@gmail.com>
2019-01-14 20:23:35 +0800

    updated apps and package

--- update_apps_list.sh
+++ update_apps_list.sh
@@ -1,3 +1,12 @@
+echo 'updating list files'
 brew leaves > brew_apps.txt
 brew cask list -1 > cask_apps.txt
 npm ls -g -depth=0 --json | jq '.dependencies | keys | .[]' | cut -d '"' -f 2 > npm_global_packages.txt
+echo 'all list files updated'
+
+brew update
+brew upgrade
+brew cask upgrade
+brew cleanup
+
+npm update -g
