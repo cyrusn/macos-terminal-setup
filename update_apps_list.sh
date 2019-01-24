e59cc2d7d2994df65d10a674e40e7982e61cff2e
cyrusn <cyrusncy@gmail.com>
2019-01-24 23:35:18 +0800

    update apps/packages list

--- update_apps_list.sh
+++ update_apps_list.sh
@@ -1,12 +1,15 @@
-echo 'updating list files'
-brew leaves > brew_apps.txt
-brew cask list -1 > cask_apps.txt
-npm ls -g -depth=0 --json | jq '.dependencies | keys | .[]' | cut -d '"' -f 2 > npm_global_packages.txt
-echo 'all list files updated'
+BASEDIR=$(dirname $0)
+
+echo 'updating apps / packages list files'
+brew leaves > ${BASEDIR}/brew_apps.txt
+brew cask list -1 > ${BASEDIR}/cask_apps.txt
+npm ls -g -depth=0 --json | jq '.dependencies | keys | .[]' | cut -d '"' -f 2 > ${BASEDIR}/npm_global_packages.txt
+echo 'updated'
 
 brew update
 brew upgrade
 brew cask upgrade
 brew cleanup
 
+npm update -g npm
 npm update -g
